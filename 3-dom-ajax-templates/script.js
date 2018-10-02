f4926a52c5d61f95f21547993fa229252ea184dd
JulianaAmoasei <juliana.amoasei@gmail.com>
2018-10-02 18:39:01 -0300

    twitterzinho implantado

--- 3-dom-ajax-templates/script.js
+++ 3-dom-ajax-templates/script.js
@@ -1,13 +1,36 @@
-`<table>
-  <thead>
-    <tr>
-      <td>país</td>
-      <td>lançamentos</td>
-    </tr>
-  </thead>
-rockets.forEach(
-  `
-  `
-  )
 
-`
+$( document ).ready(function() {
+
+  const getTweetText = $("#btn-submit").click(function(event){
+    event.preventDefault();
+    let tweet = $("#text-field").val();
+    appendTweetToList(tweet);
+    $("#text-field").val("");
+  });
+
+  function appendTweetToList(tweet) {
+    $("#tweets").prepend(
+      `<div class="tweet bg-light mt-3 p-3">
+        <p class="tweet-text h3">${tweet}</p>
+        <span id="span-date" class="small">${appendDateToTweet()}</span>
+      </div>`
+      )
+  }
+
+  function appendDateToTweet(){
+    return formatDate(new Date());
+  }
+
+  function formatDate(date) {
+    var monthNames = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out","nov", "dez"];
+    let day = date.getDate();
+    let monthIndex = date.getMonth();
+    let year = date.getFullYear();
+    return day + " de " + monthNames[monthIndex] + " de " + year;
+  }
+  
+});
+
+
+
+
