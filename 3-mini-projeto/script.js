32869d58ab04e2cefe298a5f0f41fde87ccb55e4
JulianaAmoasei <juliana.amoasei@gmail.com>
2018-10-10 16:04:36 -0300

    revisao mini-projeto com innerhtml + concat

--- 3-mini-projeto/script.js
+++ 3-mini-projeto/script.js
@@ -9,18 +9,33 @@ function getProducts(){
 }
 
 function showProducts(){
-  let productDiv = document.getElementById("products");
-  productDiv.innerHTML = `
-    ${getProducts().map((produto) => `
-      <div class="product">
-        <img src="${produto["product"]["images"][0]}" class="product-img" />
-        <div class="text-name">
-          <h3 class="product-name">${produto["product"]["name"]}</h3>
-        </div>
-        <div class="text-price">
-          <p class="product-price">${Number(produto["product"]["price"]["value"]).toLocaleString('pt-br', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' })}</p>
-        </div>
-      </div>
-      `).join("")}
-  `
+
+  for (produto of getProducts()){
+    let productDiv = document.getElementById("products");
+    productDiv.innerHTML += "<div class='product'>" 
+    + "<img src=" + produto["product"]["images"][0] + " class='product-img' />"
+    + "<div class='text-name'>" 
+    + "<h3 class='product-name'>$" + produto["product"]["name"] + "</h3>"
+    + "</div>" + "<div class='text-price'>"
+    + "<p class='product-price'>" 
+    + Number(produto["product"]["price"]["value"]).toLocaleString('pt-br', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' })
+    + "</p>" + "</div>" + "</div>"
+  }
 }
+
+
+
+  // let productDiv = document.getElementById("products");
+  // productDiv.innerHTML = 
+  //   ${getProducts().map((produto) => `
+  //     <div class="product">
+  //       <img src="${produto["product"]["images"][0]}" class="product-img" />
+  //       <div class="text-name">
+  //         <h3 class="product-name">${produto["product"]["name"]}</h3>
+  //       </div>
+  //       <div class="text-price">
+  //         <p class="product-price">${Number(produto["product"]["price"]["value"]).toLocaleString('pt-br', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' })}</p>
+  //       </div>
+  //     </div>
+  //     `).join("")}
+  // `
